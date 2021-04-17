549298899e37d6df6e82c14762b402a99dba0713
guricerin <chanbo1e9@gmail.com>
2021-04-17 16:09:05 +0900

    p.108

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,8 +1,50 @@
 #include "../MikanLoaderPkg/frame_buffer_config.h"
+#include <cstddef>
 #include <cstdint>
 
 struct PixelColor {
     uint8_t r, g, b;
+};
+
+class PixelWriter {
+private:
+    const FrameBufferConfig& _config;
+
+public:
+    PixelWriter(const FrameBufferConfig& config) : _config(config) {
+    }
+    virtual ~PixelWriter() = default;
+    virtual void Write(int x, int y, const PixelColor& color) = 0;
+
+protected:
+    uint8_t* PixelAt(int x, int y) {
+        // 1ピクセルは4バイト（rgbの24bit + 予約領域の8bit）
+        return _config.frame_buffer + 4 * (_config.pixels_per_scan_line * y + x);
+    }
+};
+
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& color) override {
+        auto p = PixelAt(x, y);
+        p[0] = color.r;
+        p[1] = color.g;
+        p[2] = color.b;
+    }
+};
+
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& color) override {
+        auto p = PixelAt(x, y);
+        p[0] = color.b;
+        p[1] = color.g;
+        p[2] = color.r;
+    }
 };
 
 /** 1つの点を描画。 
@@ -11,7 +53,6 @@ struct PixelColor {
 */
 int WritePixel(const FrameBufferConfig& config, int x, int y, const PixelColor& color) {
     const int pixel_position = config.pixels_per_scan_line * y + x;
-    // 1ピクセルは4バイト（rgbの24bit + 予約領域の8bit）
     uint8_t* p = &config.frame_buffer[4 * pixel_position];
 
     switch (config.pixel_format) {
@@ -31,19 +72,41 @@ int WritePixel(const FrameBufferConfig& config, int x, int y, const PixelColor& 
     return 0;
 }
 
+/** 配置new
+ * この時点のカーネルにはメモリ管理機能がないので普通のnewは使えない
+*/
+void* operator new(size_t size, void* buf) {
+    return buf;
+}
+
+void operator delete(void* obj) noexcept {
+}
+
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter* pixel_writer;
+
 // ブートローダからフレームバッファの情報を受け取る
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+    switch (frame_buffer_config.pixel_format) {
+    case kPixelRGBResv8BitPerColor:
+        pixel_writer = new (pixel_writer_buf) RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+        break;
+    case kPixelBGRResv8BitPerColor:
+        pixel_writer = new (pixel_writer_buf) BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+        break;
+    }
+
     // ピクセル描画
     // 画面全体を白くする
     for (int x = 0; x < frame_buffer_config.horizontal_resolution; x++) {
         for (int y = 0; y < frame_buffer_config.vertical_resolution; y++) {
-            WritePixel(frame_buffer_config, x, y, {255, 255, 255});
+            pixel_writer->Write(x, y, {255, 255, 255});
         }
     }
     // 一部を緑にする
     for (int x = 0; x < 200; x++) {
         for (int y = 0; y < 100; y++) {
-            WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+            pixel_writer->Write(x, y, {0, 255, 0});
         }
     }
 
