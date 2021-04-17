8ee8979fc488807b7d79184dfcb6b38a66a1395f
guricerin <chanbo1e9@gmail.com>
2021-04-17 20:21:28 +0900

    p.121

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -82,17 +82,50 @@ void* operator new(size_t size, void* buf) {
 void operator delete(void* obj) noexcept {
 }
 
-char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
-PixelWriter* pixel_writer;
+char g_pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter* g_pixel_writer;
+
+const uint8_t g_fontA[16] = {
+    0b00000000,
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b01111110, //  ******
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b11100111, // ***  ***
+    0b00000000, //
+    0b00000000, //
+};
+
+void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color) {
+    if (c != 'A') {
+        return;
+    }
+    for (int dy = 0; dy < 16; dy++) {
+        for (int dx = 0; dx < 16; dx++) {
+            if ((g_fontA[dy] << dx) & 0x80u) {
+                writer.Write(x + dx, y + dy, color);
+            }
+        }
+    }
+}
 
 // ブートローダからフレームバッファの情報を受け取る
-extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+extern "C" void
+KernelMain(const FrameBufferConfig& frame_buffer_config) {
     switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
-        pixel_writer = new (pixel_writer_buf) RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+        g_pixel_writer = new (g_pixel_writer_buf) RGBResv8BitPerColorPixelWriter{frame_buffer_config};
         break;
     case kPixelBGRResv8BitPerColor:
-        pixel_writer = new (pixel_writer_buf) BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+        g_pixel_writer = new (g_pixel_writer_buf) BGRResv8BitPerColorPixelWriter{frame_buffer_config};
         break;
     }
 
@@ -100,15 +133,18 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     // 画面全体を白くする
     for (int x = 0; x < frame_buffer_config.horizontal_resolution; x++) {
         for (int y = 0; y < frame_buffer_config.vertical_resolution; y++) {
-            pixel_writer->Write(x, y, {255, 255, 255});
+            g_pixel_writer->Write(x, y, {255, 255, 255});
         }
     }
     // 一部を緑にする
     for (int x = 0; x < 200; x++) {
         for (int y = 0; y < 100; y++) {
-            pixel_writer->Write(x, y, {0, 255, 0});
+            g_pixel_writer->Write(x, y, {0, 255, 0});
         }
     }
+
+    WriteAscii(*g_pixel_writer, 50, 50, 'A', {0, 0, 0});
+    WriteAscii(*g_pixel_writer, 58, 50, 'A', {0, 0, 0});
 
     while (1) {
         // CPU停止
