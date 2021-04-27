6cb697574e31977f3bb8eeef84b65b806cfb5948
guricerin <chanbo1e9@gmail.com>
2021-04-27 11:46:31 +0900

    p.292

--- kernel/acpi.cpp
+++ kernel/acpi.cpp
@@ -47,9 +47,55 @@ namespace acpi {
         return true;
     }
 
+    bool DescriptionHeader::IsValid(const char* expected_signature) const {
+        if (strncmp(this->signature, expected_signature, 4) != 0) {
+            Log(kDebug, "invalid signature: %.4\n", this->signature);
+            return false;
+        }
+        if (auto sum = SumBytes(this, this->length); sum != 0) {
+            Log(kDebug, "sum of %u bytes must be 0: %d\n", this->length, sum);
+            return false;
+        }
+        return true;
+    }
+
+    const DescriptionHeader& XSDT::operator[](size_t i) const {
+        // XSDTのDescriptor Headerの後ろは、各データ構造が連続したメモリ領域に並んでいる
+        auto entries = reinterpret_cast<const uint64_t*>(&this->header + 1);
+        // 各データ構造の先頭もDescriptor Headerなので、先頭のアドレスをDescriptor Header&にキャストする
+        return *reinterpret_cast<const DescriptionHeader*>(entries[i]);
+    }
+
+    size_t XSDT::Count() const {
+        return (this->header.length - sizeof(DescriptionHeader)) / sizeof(uint64_t);
+    }
+
+    const FADT* g_fadt;
+
     void Initialize(const RSDP& rsdp) {
         if (!rsdp.IsValid()) {
             Log(kError, "RSDP is not valid\n");
+            exit(1);
+        }
+
+        const XSDT& xsdt = *reinterpret_cast<const XSDT*>(rsdp.xsdt_address);
+        if (!xsdt.header.IsValid("XSDT")) {
+            Log(kError, "XSDT is not valid\n");
+            exit(1);
+        }
+
+        // XSDTが持つアドレス配列からFADTを検索
+        g_fadt = nullptr;
+        for (int i = 0; i < xsdt.Count(); i++) {
+            const auto& entry = xsdt[i];
+            if (entry.IsValid("FACP")) { // FACP is the signature of FADT
+                g_fadt = reinterpret_cast<const FADT*>(&entry);
+                break;
+            }
+        }
+
+        if (g_fadt == nullptr) {
+            Log(kError, "FADT is not found\n");
             exit(1);
         }
     }
--- kernel/acpi.hpp
+++ kernel/acpi.hpp
@@ -5,20 +5,14 @@
 /// 電源管理のためのタイマ。他の用途に使用してもよい
 /// Local APICとはちがって、動作周波数がわかっている
 
-/// FADT
-/// ACPI PMタイマを使う際に必要な、タイマを制御するレジスタ群のポート番号を記載しているテーブル
-
-/// XSDT : Extended System Descriptor Table
-/// FADTのメモリアドレスを記載しているテーブル
-
-/// RSDP : Root System Descriptor Pointer
-/// XSDTのメモリアドレスを記載している構造体
-
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 
 namespace acpi {
+    /// RSDP : Root System Descriptor Pointer
+    /// XSDTのメモリアドレスを記載している構造体
     struct RSDP {
         /// RSDPのシグネチャ。「RSD__PTR__」の8文字
         char signature[8];
@@ -41,6 +35,45 @@ namespace acpi {
 
         bool IsValid() const;
     } __attribute__((packed));
+
+    /// XSDT自身やXSDTから指される他のデータ構造に共通する記述ヘッダ
+    struct DescriptionHeader {
+        char signature[4];
+        uint32_t length;
+        uint8_t revision;
+        uint8_t checksum;
+        char oem_id[6];
+        char oem_table_id[8];
+        uint32_t oem_revision;
+        uint32_t creator_id;
+        uint32_t creator_revision;
+
+        bool IsValid(const char* expected_signature) const;
+    } __attribute__((packed));
+
+    /// XSDT : Extended System Descriptor Table
+    /// FADTのメモリアドレスを記載しているテーブル
+    struct XSDT {
+        DescriptionHeader header;
+
+        const DescriptionHeader& operator[](size_t i) const;
+        /// XSDTが保持するデータ構造のアドレスの個数
+        size_t Count() const;
+    } __attribute__((packed));
+
+    /// FADT
+    /// ACPI PMタイマを使う際に必要な、タイマを制御するレジスタ群のポート番号を記載しているテーブル
+    struct FADT {
+        DescriptionHeader header;
+
+        char reserved[76 - sizeof(header)];
+        uint32_t pm_tmr_blk;
+        char reserved2[112 - 80];
+        uint32_t flags;
+        char reserved3[276 - 116];
+    } __attribute__((packed));
+
+    extern const FADT* g_fadt;
 
     void Initialize(const RSDP& rsdp);
 } // namespace acpi
